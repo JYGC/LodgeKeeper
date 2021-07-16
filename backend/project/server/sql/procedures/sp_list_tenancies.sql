e83d1884d7f989a0dac8e68e57523d71773e31c1
JYGC <jygchen@gmail.com>
2021-07-16 11:22:37 +1000

    fn_tenant_names_array not needed

--- backend/project/server/sql/procedures/sp_list_tenancies.sql
+++ backend/project/server/sql/procedures/sp_list_tenancies.sql
@@ -16,54 +16,59 @@ RETURNS TABLE
 AS $$
 BEGIN
     RETURN QUERY SELECT
-        ROW_TO_JSON(tenancy.*) AS tenancy,
-        tenant_names.tenant_name_array AS tenant_names,
-        payment_terms.value AS payment_terms,
-        rent_type.value AS rent_type,
-        next_payment.next_payment_due_date AS next_payment,
-        tenancy_status.value AS tenancy_status
+        tenancy_details.*
     FROM
-        users
-    LEFT OUTER JOIN
-        tenancy
-    ON
-        users.account_id = tenancy.account_id
-    LEFT OUTER JOIN
     (
         SELECT
-            tenancy_id,
-            tenant_name_array
+            ROW_TO_JSON(tenancy.*) AS tenancy,
+            ARRAY_AGG(tenant.name) AS tenant_names,
+            payment_terms.value AS payment_terms,
+            rent_type.value AS rent_type,
+            next_payment.next_payment_due_date AS next_payment,
+            tenancy_status.value AS tenancy_status
         FROM
-            fn_tenant_names_array(p_user_id)
-    ) AS tenant_names
-    ON
-        tenancy.id = tenant_names.tenancy_id
-    LEFT OUTER JOIN
-        payment_terms
-    ON
-        tenancy.payment_terms_id = payment_terms.id
-    LEFT OUTER JOIN
-        rent_type
-    ON
-        tenancy.rent_type_id = rent_type.id
-    LEFT OUTER JOIN
-    (
-        SELECT
-            tenancy_id,
-            next_payment_due_date
-        FROM
-            fn_all_next_payment_duedates(p_user_id)
-    ) AS next_payment
-    ON
-        tenancy.id = next_payment.tenancy_id
-    LEFT OUTER JOIN
-        tenancy_status
-    ON
-        tenancy.tenancy_status_id = tenancy_status.id
-    WHERE
-        users.id = p_user_id
-    AND
-        tenancy.is_deleted = false;
+            users
+        LEFT OUTER JOIN
+            tenancy
+        ON
+            users.account_id = tenancy.account_id
+        LEFT OUTER JOIN
+            tenant
+        ON
+            tenancy.id = tenant.tenancy_id
+        LEFT OUTER JOIN
+            payment_terms
+        ON
+            tenancy.payment_terms_id = payment_terms.id
+        LEFT OUTER JOIN
+            rent_type
+        ON
+            tenancy.rent_type_id = rent_type.id
+        LEFT OUTER JOIN
+        (
+            SELECT
+                tenancy_id,
+                next_payment_due_date
+            FROM
+                fn_all_next_payment_duedates(p_user_id)
+        ) AS next_payment
+        ON
+            tenancy.id = next_payment.tenancy_id
+        LEFT OUTER JOIN
+            tenancy_status
+        ON
+            tenancy.tenancy_status_id = tenancy_status.id
+        WHERE
+            users.id = p_user_id
+        AND
+            tenancy.is_deleted = false
+        GROUP BY
+            tenancy.id,
+            payment_terms,
+            rent_type,
+            next_payment,
+            tenancy_status
+    ) AS tenancy_details;
 END; $$
 
 LANGUAGE 'plpgsql';
